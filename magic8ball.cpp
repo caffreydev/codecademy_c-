f966afbfebe2b4f287f58cc19189332279895db1
caffreydev <joe.mccaffrey@hotmail.co.uk>
2023-12-08 19:58:40 +0000

    builds a magic 8 ball

---
+++ magic8ball.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+
+int main ()
+{
+
+const char* messages[] = {
+    "It is certain.",
+"It is decidedly so.",
+"Without a doubt.",
+"Yes - definitely.",
+"You may rely on it.",
+"As I see it, yes.",
+"Most likely.",
+"Outlook good.",
+"Yes.",
+"Signs point to yes.",
+"Reply hazy, try again.",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don't count on it.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Very doubtful."};
+
+char choice;
+
+while (true)
+{
+    std::cout << "give it a shake: (Y/N)";
+    std::cin >> choice;
+    
+    if (choice == 'Y' || "y")
+    {
+        int message = rand() % (sizeof(messages) / sizeof(messages[0]));
+        std::cout << messages[message] << "\n";
+    }
+    else
+    {
+        break;
+    }
+}
+
+
+
+}
